8fd0e11731f99769b4e26942407ab53da7500f68
tanjiro-kamado49 <88613351+tanjiro-kamado49@users.noreply.github.com>
2021-12-08 07:51:51 +0530

    Time: 1 ms (5.85%), Space: 39.4 MB (25.84%) - LeetHub

---
+++ rotate-image/rotate-image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i<j){
+                   int temp=matrix[i][j];
+                   matrix[i][j]=matrix[j][i];
+                   matrix[j][i]=temp;
+                }
+            }
+        }
+        for(int j=0;j<n/2;j++){
+            for(int i=0;i<n;i++){
+                 int temp=matrix[i][n-j-1];
+                   matrix[i][n-j-1]=matrix[i][j];
+                   matrix[i][j]=temp;
+            }
+          
+        }
+        
+    }
+}
