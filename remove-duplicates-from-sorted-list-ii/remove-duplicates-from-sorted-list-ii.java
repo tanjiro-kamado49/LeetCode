2cd8664b22c1a388a9ddbdb3e65938034818b7b9
tanjiro-kamado49 <88613351+tanjiro-kamado49@users.noreply.github.com>
2021-12-14 17:45:39 +0530

    Time: 0 ms (100.00%), Space: 38.5 MB (60.05%) - LeetHub

--- remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -9,59 +9,31 @@
  * }
  */
 class Solution {
-        ListNode head1=null;
-        public ListNode insert(ListNode list, int data)
-    {
-        // Create a new node with given data
-        ListNode new_node = new ListNode(data);
-        new_node.next = null;
-   
-        // If the Linked List is empty,
-        // then make the new node as head
-        if (head1 == null) {
-            head1 = new_node;
-        }
-        else {
-            // Else traverse till the last node
-            // and insert the new_node there
-            ListNode last = head1;
-            while (last.next != null) {
-                last = last.next;
-            }
-   
-            // Insert the new_node at last node
-            last.next = new_node;
-        }
-   
-        // Return the list by head
-        return list;
-    }
     public ListNode deleteDuplicates(ListNode head) {
-            HashMap<Integer,Integer>hm=new HashMap<>();
-            ListNode temp=head;
-            while(temp!=null){
-                    if(hm.containsKey(temp.val)==true)
-                            hm.put(temp.val,hm.get(temp.val)+1);
-                    else
-                            hm.put(temp.val,1);
-                    temp=temp.next;
-            }
-            List<Integer>arr=new ArrayList<>();
-              for(Map.Entry m : hm.entrySet()){    
-                   if((int)m.getValue()==1){
-                           arr.add((int)m.getKey());
-                   }     
-              }
-            System.out.println(arr);
-            Collections.sort(arr);
-           ListNode list = new ListNode();
-            for(int i=0;i<arr.size();i++){
-                    list=insert(head1,  arr.get(i));
-               
+            ListNode sentinel =new ListNode(0,head);
+            ListNode pred=sentinel;
+            while(head!=null){
+                    //System.out.println("head---"+head.val);
+                    //System.out.println("pred---"+pred.val);
+                    //System.out.println("sentinel.next-----"+sentinel.next.val);
+                    if(head.next!=null && head.val==head.next.val){
+                            
+                            while(head.next!=null && head.next.val==head.val){
+                                    //System.out.println("head---"+head.val);
+                                    head=head.next;
+                            }
+                            pred.next=head.next;
+                            //System.out.println("pred.next---"+pred.val);
+                    }
+                    else{   
+                            pred=pred.next;
+                            //System.out.println("pred---"+pred.val);
+                    }
+                    
+                    head=head.next;
+                     //System.out.println("---------------------------");
                     
             }
-            
-            return head1;
-        
+        return sentinel.next;
     }
 }
