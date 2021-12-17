5a696634ce140423dc68b37ad2b2f11c985c2dbd
tanjiro-kamado49 <88613351+tanjiro-kamado49@users.noreply.github.com>
2021-12-17 12:47:42 +0530

    Time: 13 ms (5.19%), Space: 39.8 MB (5.19%) - LeetHub

--- rings-and-rods/rings-and-rods.java
+++ rings-and-rods/rings-and-rods.java
@@ -1,19 +1,22 @@
 class Solution {
     public int countPoints(String rings) {
             HashMap<Integer,String>hm=new HashMap<>();
-            for(int i=0;i<rings.length();i=i+2){
-                    int num=Character.getNumericValue(rings.charAt(i+1));
-                    char color=rings.charAt(i);
-                    hm.put(num,hm.getOrDefault(num,"")+color);
+            for(int i=1;i<rings.length();i++){
+                    char x=rings.charAt(i-1);
+                    int y=Character.getNumericValue(rings.charAt(i));
                     
+                            hm.put(y,hm.getOrDefault(y,"")+x);
             }
+          
             int count=0;
-               for (Map.Entry<Integer,String> map : hm.entrySet()) {
-                       if(map.getValue().contains("B") && map.getValue().contains("R")&&map.getValue().contains("G")){
+            for(Map.Entry<Integer,String>  map:hm.entrySet()){
+                if(map.getValue().contains("B") && map.getValue().contains("R")&&map.getValue().contains("G")){
                                count++;
                        }
-        }
-                       return count;
+                    
+                    
+            }
+            return count;
         
     }
 }
