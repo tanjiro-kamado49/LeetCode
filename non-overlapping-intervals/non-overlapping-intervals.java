38d7b0bc54d7140a016f5f0cf422665163cfbaff
tanjiro-kamado49 <88613351+tanjiro-kamado49@users.noreply.github.com>
2021-12-23 13:22:24 +0530

    Time: 1626 ms (5.03%), Space: 78.9 MB (99.87%) - LeetHub

--- non-overlapping-intervals/non-overlapping-intervals.java
+++ non-overlapping-intervals/non-overlapping-intervals.java
@@ -5,13 +5,18 @@ class Solution {
             int prev_e=intervals[0][1];
             int ans=0;
             for(int i=1;i<intervals.length;i++){
+                     System.out.println(i);
+                    System.out.println("prev_s"+prev_s);
+                    System.out.println("prev_e"+prev_e);
+                     
                     int new_s=intervals[i][0];
                     int new_e=intervals[i][1];
-                    if(new_s<=prev_s || new_s<prev_e) ans++;
+                    if( new_s<prev_e) ans++;
                     else{
                             prev_s=new_s;
                             prev_e=new_e;
                     }
+                    System.out.println("ans"+ans);
             }
             return ans;
         
