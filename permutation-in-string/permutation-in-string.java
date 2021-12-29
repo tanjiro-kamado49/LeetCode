25e2cb6039e390b92853c63331b71d410fdad494
tanjiro-kamado49 <88613351+tanjiro-kamado49@users.noreply.github.com>
2021-12-29 10:42:44 +0530

    Time: 4 ms (84.28%), Space: 38.8 MB (86.74%) - LeetHub

---
+++ permutation-in-string/permutation-in-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+            if(s1.length()>s2.length() || s2.length()==0) return false;
+            int arr1[]=new int[26];
+            int arr2[]=new int[26];
+            for(int i=0;i<s1.length();i++){
+                    arr1[s1.charAt(i)-'a']++;
+                    arr2[s2.charAt(i)-'a']++;
+            }
+            for(int j=s1.length();j<s2.length();j++){
+                   if( Arrays.equals(arr1,arr2) )return true;
+                    arr2[s2.charAt(j)-'a']++;
+                    arr2[s2.charAt(j-s1.length())-'a']--;
+                    
+            }
+            if(Arrays.equals(arr1,arr2)) return true;
+            return false;
+
+        
+    }
+}
