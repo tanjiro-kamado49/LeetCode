645145868a7ff1836cc21add1e9465014c1a5e3b
tanjiro-kamado49 <88613351+tanjiro-kamado49@users.noreply.github.com>
2021-12-30 23:59:55 +0530

    Time: 3 ms (92.08%), Space: 39.5 MB (22.28%) - LeetHub

--- multiply-strings/multiply-strings.java
+++ multiply-strings/multiply-strings.java
@@ -1,8 +1,25 @@
 class Solution {
     public String multiply(String num1, String num2) {
-           java.math.BigInteger n1 =new java.math.BigInteger(num1);
-            java.math.BigInteger n2=new java.math.BigInteger(num2);
-            return String.valueOf(n1.multiply(n2));
-        
+          if(num1.equals("0") || num2.equals("0"))
+                  return "0";
+          StringBuffer sb = new StringBuffer();
+            int arr[]=new int[num1.length()+num2.length()];
+            for(int i=num1.length()-1;i>=0;i--){
+                   int n1=num1.charAt(i)-'0';
+                    for(int j=num2.length()-1;j>=0;j--){
+                            int n2=num2.charAt(j)-'0';
+                            int sum=arr[i+j+1]+n1*n2;
+                            arr[i+j+1]=sum%10;
+                            arr[i+j]+=sum/10;
+                    }
+            }
+            int index=0;
+            if(arr[0]==0){
+                    index=1;
+            }
+            while(index<arr.length){
+                    sb.append(arr[index++]);
+            }
+            return sb.toString();
     }
 }
