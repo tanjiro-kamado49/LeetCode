6c7af8a580cb720134af1d39d47b3f2637346c5f
tanjiro-kamado49 <88613351+tanjiro-kamado49@users.noreply.github.com>
2022-02-08 11:14:13 +0530

    Time: 2 ms (59.15%), Space: 41.2 MB (11.57%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -1,19 +1,9 @@
 class Solution {
     public int addDigits(int num) {
-            if(num<=9) return num;
-            int cur=num;
-            int sum=0;
-            while(true){
-                    while(cur!=0){
-                            int rem=cur%10;
-                            sum+=rem;
-                            cur=cur/10;
-                    }
-                    if(sum<=9) return sum;
-                    cur=sum;
-                    sum=0;
-            }
-            // return 0;
+             if(num==0) return 0;
+        else if(num%9==0) return 9;
+        else if(num%9!=0) return num%9;
+            return 0;
         
     }
 }
