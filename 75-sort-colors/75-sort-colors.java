9d3843d37bdd1dd52eb0465749cd4d1998591e4f
tanjiro-kamado49 <88613351+tanjiro-kamado49@users.noreply.github.com>
2022-05-09 22:16:48 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (50.88%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,27 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-            int n0=0;
-            int n1=0;
-            int n2=0;
-            for(int i=0;i<nums.length;i++){
-                    if(nums[i]==0) n0++;
-                    if(nums[i]==1) n1++;
-                    if(nums[i]==2) n2++;
-            }
-            // System.out.println(n0,)
-            int k=0;
-            for(k=0;k<n0;k++){
-                    nums[k]=0;
-            }
-            int j=k+n1;
-            for(;k<j;k++){
-                    nums[k]=1;
-                    
-            }
-            j=k+n2;
-            for(;k<j;k++){
-                    nums[k]=2;
-            }
-            
+            int mid=0;
+            int low=0;
+            int high=nums.length-1;
+           
+            while(mid<=high){
+                    if(nums[mid]==0){ 
+                            int t=nums[low];
+                            nums[low]=nums[mid];
+                            nums[mid]=t;
+                            low++;
+                            mid++;
+                    }
+                    else if(nums[mid]==1){
+                            mid++;
+                    }
+                    else if(nums[mid]==2){
+                            int t=nums[mid];
+                            nums[mid]=nums[high];
+                            nums[high]=t;
+                            high--;
+                    }
+            }            
+        
     }
 }
