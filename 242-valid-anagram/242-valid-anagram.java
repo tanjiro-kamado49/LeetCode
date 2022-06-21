2ddc2b2cfe226fd3ea1d094902ffd2fee4d12646
tanjiro-kamado49 <88613351+tanjiro-kamado49@users.noreply.github.com>
2022-06-22 01:17:39 +0530

    Time: 6 ms (60.71%), Space: 43.1 MB (78.30%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -4,9 +4,7 @@ class Solution {
         int[]freq=new int[26];
         for(int i=0;i<s.length();i++){
             freq[s.charAt(i)-'a']++;
-        }
-         for(int i=0;i<s.length();i++){
-            freq[t.charAt(i)-'a']--;
+              freq[t.charAt(i)-'a']--;
         }
         for(int i=0;i<26;i++){
             if(freq[i]!=0) return false;
