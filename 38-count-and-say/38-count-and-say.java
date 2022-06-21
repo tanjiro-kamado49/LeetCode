a16e196f3432b3892a62e1d7d0c9227174167d80
tanjiro-kamado49 <88613351+tanjiro-kamado49@users.noreply.github.com>
2022-06-22 00:59:49 +0530

    Time: 56 ms (7.81%), Space: 67.6 MB (6.25%) - LeetHub

--- 38-count-and-say/38-count-and-say.java
+++ 38-count-and-say/38-count-and-say.java
@@ -1,52 +1,52 @@
-// class Solution {
-//     public String countAndSay(int n) {
-//         if(n==1) return "1";
-//         if(n==2) return "11";
-//         String s=countAndSay(n-1);
-//             String t="";
-//             int c=1;
-//             s=s+'$';
-//             for(int j=1;j<s.length();j++){
-//                 if(s.charAt(j)!=s.charAt(j-1)){
-//                     t+= String.valueOf(c);
-//                     t+=s.charAt(j-1);
-//                     c=1;
-//                 }
-//                 else c++;
-//             }
-//             s=t;
-//         return s;  
-//     }
-// }
-
-class Solution 
-{
-    public String countAndSay(int n) 
-    {
-        if(n==1) 
-        { 
-            return "1";
-        }
-        String str=countAndSay(n-1);
-        StringBuilder ans=new StringBuilder();
-        char prev=str.charAt(0); 
-        int count=1;
-        for(int i=1;i<str.length();i++) 
-        {
-            if(str.charAt(i)==prev) 
-            {
-                count++;
-            } 
-            else 
-            {
-                ans.append(count);
-                ans.append(prev);
-                prev=str.charAt(i);
-                count=1;
+class Solution {
+    public String countAndSay(int n) {
+        if(n==1) return "1";
+        if(n==2) return "11";
+        String s=countAndSay(n-1);
+            String t="";
+            int c=1;
+            s=s+'$';
+            for(int j=1;j<s.length();j++){
+                if(s.charAt(j)!=s.charAt(j-1)){
+                    t+= String.valueOf(c);
+                    t+=s.charAt(j-1);
+                    c=1;
+                }
+                else c++;
             }
-        }
-        ans.append(count);
-        ans.append(prev); 
-        return ans.toString();
+            s=t;
+        return s;  
     }
 }
+
+// class Solution 
+// {
+//     public String countAndSay(int n) 
+//     {
+//         if(n==1) 
+//         { 
+//             return "1";
+//         }
+//         String str=countAndSay(n-1);
+//         StringBuilder ans=new StringBuilder();
+//         char prev=str.charAt(0); 
+//         int count=1;
+//         for(int i=1;i<str.length();i++) 
+//         {
+//             if(str.charAt(i)==prev) 
+//             {
+//                 count++;
+//             } 
+//             else 
+//             {
+//                 ans.append(count);
+//                 ans.append(prev);
+//                 prev=str.charAt(i);
+//                 count=1;
+//             }
+//         }
+//         ans.append(count);
+//         ans.append(prev); 
+//         return ans.toString();
+//     }
+// }
