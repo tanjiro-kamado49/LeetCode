ff11bb34fce56b9eb687886b993aa6da38a04ec6
tanjiro-kamado49 <88613351+tanjiro-kamado49@users.noreply.github.com>
2022-06-28 06:43:03 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (16.30%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -14,31 +14,10 @@
  * }
  */
 class Solution {
-    public void func(TreeNode p,TreeNode q,boolean[]ans){
-        if(p==null && q==null){
-            return ;
-        }
-        else if((p==null && q!=null) ||(p!=null && q==null)) {
-            ans[0]=false;
-            return;
-        }
-        
-        else if(p.val==q.val){
-            func(p.left,q.left,ans);
-            func(p.right,q.right,ans);
-            
-        }
-        else{
-            ans[0]=false;
-            return;
-        }
-    }
-    
     public boolean isSameTree(TreeNode p, TreeNode q) {
-       boolean[]ans=new boolean[1];
-        ans[0]=true;
-        func(p,q,ans);
-        return ans[0];
+        if(p==null || q==null) return (p==q);
+        return  (p.val==q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        
     
         
     }
