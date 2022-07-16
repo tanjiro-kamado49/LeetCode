e003f44d8162e94538ace2e807b08188ff5943a0
tanjiro-kamado49 <88613351+tanjiro-kamado49@users.noreply.github.com>
2022-07-16 21:30:06 +0530

    Time: 1531 ms (14.09%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -7,13 +7,3 @@ end as'travelled_distance'
 from users u left join rides  r on u.id=r.user_id
 group by r.user_id 
 order by travelled_distance desc, u.name;
-
-# SELECT U.NAME AS 'name',
-# CASE 
-# WHEN U.ID NOT IN (SELECT USER_ID FROM RIDES) THEN 0
-# ELSE
-# 	SUM(R.DISTANCE) 
-# END AS 'travelled_distance'
-# FROM USERS U LEFT JOIN RIDES R ON U.ID = R.USER_ID
-# GROUP BY R.USER_ID
-# ORDER BY travelled_distance DESC,U.NAME
